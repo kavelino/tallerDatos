de78f5930e11f35d5b2aa33b61e02a780cd2216d
kavelino <kleberth@icloud.com>
2019-10-26 20:51:24 -0500

    Generar userid

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <stdlib.h>
 #include "./validar.h"
 
 void crearUsuario(Usuario_t *);
@@ -9,12 +10,17 @@ int main() {
 	Usuario_t user;
 	printf("Ingrese su nombre: ");
 	scanf("%s", user.nombre);
-	user->nombre[0] = toupper(user->nombre[0]);
+	user.nombre[0] = toupper(user.nombre[0]);
 	printf("Ingrese su apellido: ");
 	scanf("%s", user.apellido);
-	user->apellido[0] = toupper(user->apellido[0]);
+	user.apellido[0] = toupper(user.apellido[0]);
 	crearUsuario(&user);
 	crearPassWord(&user);
+	user->userid = 100000 + rand() % (999999 - 100000);	
+	printf("Nombre: %s %s\n", user.nombre, user.apellido);
+	printf("Usuario: %s\n", user->username);
+	printf("Password: %s\n", user->password);
+	printf("UserId: %s\n", user->userid);
 }
 
 void crearUsuario(Usuario_t *user) {
@@ -28,17 +34,37 @@ void crearUsuario(Usuario_t *user) {
 			break;
 		}
 	}
-	printf("Usuario sugerido: %s", user->username);
 }
 
 void crearPassWord(Usuario_t *user) {
 	int boolean = 1;
 
-	while(tmp) {
-		printf("Ingrese un password: ")
-		scanf("%s", user.password);
-		int tmp = validar(&user); 
-}
+	while(boolean) {
+		printf("Ingrese un password: ");
+		scanf("%s", user->password);
+		int tmp = validar(user);
 
+		switch(tmp) {
+			case 0:
+				printf("Contraseña aceptada\n");
+    				boolean = 0;
+    				break;
+			case 3:
+				printf("Password tiene menos de 10 caracteres\n");
+    				boolean = 1;
+    				break;
 
+			case 4:
+				printf("Password no contiene letras\n");
+    				boolean = 1;
+    				break;
+			case 5:
+				printf("Password no contiene digitos\n");
+				boolean = 1;
+				break;
+			default:
+				boolean = 1;
+				break;
+		}
+	}
 }
--- validar.c
+++ validar.c
@@ -1,9 +1,34 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 #include "./validar.h"
 
-#define SIZE 30
+int validar(Usuario_t *user) {
+	if(strlen(user->password) < 10) {
+		return 3;
+	}
 
-int validar(Usuario_t * user) {
+	int contChar = 0;
+	int contNum = 0;
+
+	for(int i=0; i<strlen(user->password); i++) {
+		char tmp = user->password[i];
+		if(isalpha(tmp)) {
+			contChar++;
+		}
+		else {
+			contNum++;
+		}
+	}
+
+	if(contChar == 0) {
+		return 4;
+	}
+
+	if(contNum == 0) {
+		return 5;
+	}
+
 	return 0;
 }
 
