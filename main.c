11de2e68a9c063fc894f4b00f48c575d7ac419d4
kavelino <kleberth@icloud.com>
2019-10-31 23:36:49 -0500

    Implementando funciones

--- main.c
+++ main.c
@@ -8,20 +8,39 @@ void crearPassWord(Usuario_t *);
 
 int main() {
 	Usuario_t user;
-	printf("Ingrese su nombre: ");
-	scanf("%s", user.nombre);
-	user.nombre[0] = toupper(user.nombre[0]);
-	printf("Ingrese su apellido: ");
-	scanf("%s", user.apellido);
-	user.apellido[0] = toupper(user.apellido[0]);
-	crearUsuario(&user);
-	crearPassWord(&user);
-	user.userid = 100000 + rand() % (999999 - 100000);
+	int opcion = 1;
+	while(opcion) {		
+		printf("\nIngresar usuario (1) o ");
+		printf("Salir (2): ");
+		scanf("%d", &opcion);
+	
+		
+		switch(opcion) {
+			case 1:
+				printf("Ingrese su nombre: ");
+				scanf("%s", user.nombre);
+				user.nombre[0] = toupper(user.nombre[0]);
+				printf("Ingrese su apellido: ");
+				scanf("%s", user.apellido);
+				user.apellido[0] = toupper(user.apellido[0]);
+				crearUsuario(&user);
+				crearPassWord(&user);
+				user.userid = 10000 + rand() % (99999 - 10000);
 
-	printf("\nNombre: %s %s\n", user.nombre, user.apellido);
-	printf("Usuario: %s\n", user.username);
-	printf("Password: %s\n", user.password);
-	printf("UserId: %d\n", user.userid);
+				printf("\nNombre: %s %s\n", user.nombre, user.apellido);
+				printf("Usuario: %s\n", user.username);
+				printf("Password: %s\n", user.password);
+				printf("UserId: %d\n", user.userid);
+				opcion = 1;
+				break;
+			case 2:
+				printf("Saliendo...\n");
+				opcion = 0;
+				break;
+		}
+	}
+	
+	
 }
 
 void crearUsuario(Usuario_t *user) {
